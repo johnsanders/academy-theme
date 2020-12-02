8012e1beb92725a5edf7d0857e665b5bbca5f118
John Sanders <john.sanders@warnermedia.com>
2020-12-02 15:10:24 +0000

    Move navbar to react

--- layout/columns2.php
+++ layout/columns2.php
@@ -20,9 +20,22 @@ $buildregionmainsettings = !$PAGE->include_region_main_settings_in_header_action
 $regionmainsettingsmenu = $buildregionmainsettings ? $OUTPUT->region_main_settings_menu() : false;
 $is_front_page = $PAGE->has_set_url() && $PAGE->url->compare(context_system::instance()->get_url(), URL_MATCH_BASE);
 $grid_config = get_grid_context();
+$sitename = format_string($SITE->shortname, true, ['context' => context_course::instance(SITEID), "escape" => false]);
 
+$nav_config = json_encode([
+	"ariaLabel" => get_string("sitemenubar", "admin"),
+	"customMenu" => $OUTPUT->custom_menu(),
+	"isLoggedIn" => isloggedin(),
+	"logoUrl" => (string) $OUTPUT->get_compact_logo_url(),
+	"menuButtonName" => get_string("sidepanel", "core"),
+	"navDrawerOpen" => $navdraweropen,
+	"navbarPluginOutput" => $OUTPUT->navbar_plugin_output(),
+	"pageHeadingMenu" => $OUTPUT->page_heading_menu(),
+	"siteName" => $sitename,
+	"userMenu" => $OUTPUT->user_menu(),
+]);
 $templatecontext = [
-	'sitename' => format_string($SITE->shortname, true, ['context' => context_course::instance(SITEID), "escape" => false]),
+	'sitename' => $sitename,
 	'output' => $OUTPUT,
 	'sidepreblocks' => $blockshtml,
 	'hasblocks' => $hasblocks,
@@ -35,14 +48,26 @@ $templatecontext = [
 	'rows_json' => $grid_config["rows_json"],
 	'carousel_json' => $grid_config["carousel_json"],
 	'js_src' => $grid_config["js_src"],
+	'nav_config' => $nav_config,
 ];
 
-$navAll = $PAGE->flatnav;
+$nav_all = $PAGE->flatnav;
 $nav = [];
-$unwantedNavs = ['Private files', 'Content bank'];
-foreach ($navAll as $navItem) {
-	if (!in_array($navItem->text, $unwantedNavs)) array_push($nav, $navItem);
+$unwanted_navs = ['Private files', 'Content bank', 'Badges', 'Competencies'];
+foreach ($nav_all as $nav_item) {
+	if (!in_array($nav_item->text, $unwanted_navs)) array_push(
+		$nav,
+		[
+			"action" => $nav_item->action,
+			"get_indent" => $nav_item->get_indent(),
+			"icon" => $nav_item->icon,
+			"is_section" => $nav_item->is_section,
+			"isactive" => $nav_item->isactive,
+			"text" => $nav_item->text,
+		],
+	);
 }
+
 $templatecontext['flatnavigation'] = $nav;
-$templatecontext['firstcollectionlabel'] = $navAll->get_collectionlabel();
+$templatecontext['firstcollectionlabel'] = $nav_all->get_collectionlabel();
 echo $OUTPUT->render_from_template('theme_academy/columns2', $templatecontext);
