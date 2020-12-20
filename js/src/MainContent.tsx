d529458a9b636da6051adcf68bbcfdcb8d4db37d
John Sanders <john.sanders@warnermedia.com>
2020-12-20 11:05:46 +0100

    Don't show loggedin content until loaded'

--- js/src/MainContent.tsx
+++ js/src/MainContent.tsx
@@ -2,6 +2,7 @@ import Footer from './shared/Footer';
 import Loading from './shared/Loading';
 import Navbar from './navbar';
 import React from 'react';
+import clearLoader from './helpers/clearLoader';
 
 const Settings = React.lazy(() => import('./themeSettings'));
 const FrontPage = React.lazy(() => import('./frontPage'));
@@ -9,6 +10,7 @@ const FrontPage = React.lazy(() => import('./frontPage'));
 const MainContent: React.FC = (): JSX.Element => {
 	const isSettingsPage = window.location.search.includes('section=themesettingacademy');
 	const [loading, setLoading] = React.useState(!isSettingsPage);
+	React.useEffect(clearLoader, []);
 	return (
 		<>
 			<Navbar />
