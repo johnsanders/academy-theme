fb6cc75f6530e953007ffd09de9308b92862227f
John Sanders <john.sanders@warnermedia.com>
2021-01-07 10:08:41 +0000

    Exclude all unnecessary files in deploy

--- js/deploy.sh
+++ js/deploy.sh
@@ -2,7 +2,25 @@
 
 /bin/rm ./.DS_Store
 cd ../../
-/usr/bin/tar --exclude='./.git' --exclude='./js/node_modules' --exclude='./js/src' -czvf academy.tar.gz ./academy
+/usr/bin/tar \
+	--exclude='./.git*' \
+	--exclude='./.DS_Store' \
+	--exclude='./js/.babelrc' \
+	--exclude='./js/.eslintrc.js' \
+	--exclude='./js/.prettierrc.js' \
+	--exclude='./js/.gitignore' \
+	--exclude='./js/.storybook' \
+	--exclude='./js/deploy_rollbar_maps.js' \
+	--exclude='./js/deploy.sh' \
+	--exclude='./js/node_modules' \
+	--exclude='./js/package-lock.json' \
+	--exclude='./js/package.json' \
+	--exclude='./js/rollbarToken_blank.js' \
+	--exclude='./js/rollbarToken.js' \
+	--exclude='./js/src' \
+	--exclude='./js/tsconfig.json' \
+	--exclude='./js/webpack.config.js' \
+	-czvf academy.tar.gz ./academy
 /usr/bin/scp academy.tar.gz cnnitouch@academy.cnntoolbox.com:/home/cnnitouch
 /bin/rm ./academy.tar.gz
 
